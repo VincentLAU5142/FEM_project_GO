ae4dfe15b75a63408786a1f435fb960d73fc84bd
Vincent Lau <kahung1009@gmail.com>
2025-10-25 22:30:35 +0800

    Validating User Data

--- internal/api/user_handler.go
+++ internal/api/user_handler.go
@@ -1,10 +1,19 @@
 package api
 
 import (
+	"errors"
 	"log"
+	"regexp"
 
 	"github.com/VincentLAU5142/FEM_project_GO/internal/store"
 )
+
+type registerUserRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Bio      string `json:"bio"`
+}
 
 type UserHandler struct {
 	userStore store.UserStore
@@ -16,4 +25,30 @@ func NewUserHandler(userStore store.UserStore, logger *log.Logger) *UserHandler 
 		userStore: userStore,
 		logger:    logger,
 	}
+}
+
+func (h *UserHandler) validateRegisterRequest(req *registerUserRequest) error {
+	if req.Username == "" {
+		return errors.New("username is required")
+	}
+
+	if len(req.Username) > 50 {
+		return errors.New("username cannot be greater than 50 characters")
+	}
+
+	if req.Email == "" {
+		return errors.New("email is required")
+	}
+
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !emailRegex.MatchString(req.Email) {
+		return errors.New("invalid email format")
+	}
+
+	//we can have password validation as well
+	if req.Password == "" {
+		return errors.New("password is required")
+	}
+
+	return nil
 }
